596928e4ed9086a7ad58b90cef3a1bd9ca83795c
Marmik Panchal <marmik.panchal@gmail.com>
2020-03-26 00:16:28 +1100

    Updates

---
+++ precache-manifest.80f88ca9ab38fd0d00d59e853a7a1f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e969f5260bc458ac3f80f4677e016527",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8170e0667bb708bb40d3",
+    "url": "/monsters-rolodex/static/css/main.40c75cbb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8170e0667bb708bb40d3",
+    "url": "/monsters-rolodex/static/js/main.6546dbf8.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
